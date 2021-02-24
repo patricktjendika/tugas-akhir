7288de1afbce8fb8309cfd41601d0695ec0a6e2e
Patrick Tjendika <patricktjendika2521999@gmail.com>
2021-02-24 16:55:47 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template
+app = Flask(__name__)
+
+run_with_ngrok(app)   #starts ngrok when the app is run
+@app.route("/")
+def index():
+    return "<h1>Running Flask on Google Colab WAW!</h1>"
+
+@app.route("/test/<param>")
+def test(param):
+  return "<h1>Hello "+param+"!</h1>"
+
+@app.route("/template")
+def template():
+  return render_template("index.html")
+
+app.run()
