36ef49477956b2e3c0ed07126c403f9fc3033d14
Patrick Tjendika <patricktjendika2521999@gmail.com>
2021-02-25 02:38:07 +0700

    Update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask_ngrok import run_with_ngrok
+# from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template
 app = Flask(__name__)
 
-run_with_ngrok(app)   #starts ngrok when the app is run
+# run_with_ngrok(app)   #starts ngrok when the app is run
 @app.route("/")
 def index():
     return "<h1>Running Flask on Google Colab WAW!</h1>"
