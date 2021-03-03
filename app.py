3ab8ecd89bcd3f19442009f93d2ec4a719d4017f
Patrick Tjendika <patricktjendika2521999@gmail.com>
2021-03-03 19:16:47 +0700

    Add template and data

--- app.py
+++ app.py
@@ -2,17 +2,13 @@ from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template
 app = Flask(__name__)
 
-run_with_ngrok(app)   #starts ngrok when the app is run
-@app.route("/")
-def index():
-    return "<h1>Running Flask on Google Colab WAW!</h1>"
-
+# run_with_ngrok(app)   #starts ngrok when the app is run
 @app.route("/test/<param>")
 def test(param):
   return "<h1>Hello "+param+"!</h1>"
 
-@app.route("/template")
-def template():
+@app.route("/")
+def index():
   return render_template("index.html")
 
 if __name__ == "__main__":
